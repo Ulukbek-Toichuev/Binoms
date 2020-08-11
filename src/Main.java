1487d926647968c989770575520d737fbd817872
Toychuev. Ulukbek <67442103+Ulukbek-Toychuev@users.noreply.github.com>
2020-08-11 15:54:53 +0300

    Example

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,4 @@
-class Box{
-    double width;
-    double height;
-    double depth;
-
-    Box(double w, double h, double d){
-        width = w;
-        height = h;
-        depth = d;
-    }
-
-    double volume(){
-        return width*height*depth;
-    }
-}
-
 public class Main{
     public static void main(String[] args) {
-        Box box1 = new Box(10,20,15);
-        Box box2 = new Box(3,6,9);
-
-        double vol;
-
-        vol = box1.volume();
-        System.out.println("Объем равен: " + vol);
-        vol = box2.volume();
-        System.out.println("Объем равен: " + vol);
     }
 }
